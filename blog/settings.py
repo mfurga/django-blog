a0cb985e2e13ba7495c8fcd9af81fac80bafa67a
mfurga <mfurga@protonmail.com>
2018-07-13 12:50:17 +0200

    Updated posts app. Added models.py, admin.py & base.html template. Updated settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'k*(mk!5z7e5@!w87)9-0$3g_q#d7h%ob#lg2y2&7v+fy=n**k3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Custom apps
+    'posts',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'common')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    tags = models.CharField(max_length=50)
+    publish = models.DateTimeField()
+    active = models.BooleanField(default=True)
+
+    slug = models.SlugField(unique=True, blank=True, null=True)
+
+    class Meta:
+        ordering = ['-publish']
+
+    def __str__(self):
+        return self.title

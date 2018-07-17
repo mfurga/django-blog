b5b4172343fbee74c98bcdd7dda32670153fef94
mfurga <mfurga@protonmail.com>
2018-07-17 18:42:51 +0200

    Added posts templates. Updated posts views.py

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.db.models import Q
 
 from .models import Post

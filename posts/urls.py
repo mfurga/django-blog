dd5553ef7dc78a26df67390a19f6f97fea2ba3a1
mfurga <mfurga@protonmail.com>
2018-07-18 11:40:23 +0200

    Created posts create view. Updated urls.py & base.html

--- posts/urls.py
+++ posts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import posts_list, posts_detail
+from .views import posts_list, posts_detail, posts_create
 
 
 app_name = 'posts'
 urlpatterns = [
     path('', posts_list, name='list'),
-    path('post/<slug:slug>', posts_detail, name='detail')
+    path('post/<slug:slug>', posts_detail, name='detail'),
+    path('create', posts_create, name='create')
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,12 @@
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 from django.db.models import Q
 
+import datetime
 from .models import Post
+from .forms import PostForm
 
 
 def posts_list(request):
@@ -20,3 +24,39 @@ def posts_list(request):
 def posts_detail(request, slug=None):
     qs = get_object_or_404(Post, slug=slug)
     return render(request, 'posts/posts_detail.html', {'ins': qs})
+
+
+@login_required
+def posts_create(request):
+
+    if request.method == 'POST':
+        form = PostForm(request.POST, 
+            initial={
+                'author': request.user,
+                'publish': datetime.date.today
+            }
+        )
+        if form.is_valid():
+            author = request.user
+            title = form.cleaned_data['title']
+            content = form.cleaned_data['content']
+            tags = form.cleaned_data['tags']
+            publish = form.cleaned_data['publish']
+            active = form.cleaned_data['active']
+
+            post = Post.objects.create(
+                    author=author,
+                    title=title,
+                    content=content,
+                    tags=tags,
+                    publish=publish,
+                    active=active
+                )
+
+            messages.success(request, 'Post successfully added.')
+            return redirect('posts:list')
+
+    else:
+        form = PostForm(initial={'author': request.user, 'publish': datetime.date.today}) 
+
+    return render(request, 'posts/posts_create.html', {'form': form})

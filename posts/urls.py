9ca546d6a2da76186cf1a3c71a1df379b1e8585c
mfurga <mfurga@protonmail.com>
2018-07-20 12:34:52 +0200

    Created posts delete view. Updated urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import posts_list, posts_detail, posts_create, posts_edit
+from .views import posts_list, posts_detail, posts_create, posts_edit, posts_delete
 
 
 app_name = 'posts'
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', posts_list, name='list'),
     path('post/<slug:slug>', posts_detail, name='detail'),
     path('post/<slug:slug>/edit', posts_edit, name='edit'),
+    path('post/<slug:slug>/delete', posts_delete, name='delete'),
     path('create', posts_create, name='create')
 ]
--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,9 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
+from django.utils.http import is_safe_url
 from django.contrib import messages
+from django.urls import reverse
 from django.db.models import Q
 
 import datetime
@@ -75,3 +77,15 @@ def posts_edit(request, slug=None):
         messages.success(request, 'Post successfully updated.')
         return redirect('posts:detail', slug=slug)
     return render(request, 'posts/posts_create.html', {'form': form})
+
+
+def posts_delete(request, slug=None):
+    if request.user != Post.objects.get(slug=slug).author and not request.user.is_superuser:
+        raise Http404()
+
+    redirect_to = request.GET.get('next', '')
+    if not is_safe_url(url=redirect_to, host=request.get_host()):
+        redirect_to = reverse('posts:list')
+    qs = get_object_or_404(Post, slug=slug).delete()
+    messages.success(request, 'Post successfully deleted.')
+    return HttpResponseRedirect(redirect_to)

9ea61aa1c0058113841b6768087ca128f109a9a4
mfurga <mfurga@protonmail.com>
2018-07-25 09:55:25 +0200

    Added aboutme contact templates. Created aboutme models

--- aboutme/models.py
+++ aboutme/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    email = models.EmailField()
+    message = models.TextField()
+    date = models.DateTimeField()
+
+    class Meta:
+        ordering = ['-date']
+
+    def __str__(self):
+        return self.email

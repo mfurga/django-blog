e370c3477059e525ffb6eef6af4bf4381c4cca82
mfurga <mfurga@protonmail.com>
2018-08-07 09:17:20 +0200

    Created users dashboard view. Updated urls.py & base.html

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import users_signin, users_register, users_logout
+from .views import users_signin, users_register, users_logout, users_dashboard, users_action
 
 app_name = 'users'
 urlpatterns = [
     path('signin', users_signin, name='signin'),
     path('create', users_register, name='register'),
-    path('logout', users_logout, name='logout')
+    path('logout', users_logout, name='logout'),
+    path('dashboard', users_dashboard, name='dashboard'),
+    path('action/<int:pk>/<int:action>', users_action, name='action')
 ]
--- users/views.py
+++ users/views.py
@@ -1,13 +1,16 @@
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, Http404
-from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.utils.http import is_safe_url
 from django.contrib import messages
 from django.urls import reverse
 
 from .forms import SigninForm, RegisterForm, BlogSettingsForm
+from aboutme.models import Contact
+from .models import BlogSettings
+from posts.models import Post
 
 
 def users_signin(request):
@@ -49,6 +52,50 @@ def users_register(request):
             return redirect('users:signin')
 
     return render(request, 'users/users_register.html', {'form': form})
+
+
+@login_required
+def users_dashboard(request):
+    messages_list = Contact.objects.all()
+    posts_list = Post.objects.all()
+    users_list = User.objects.all()
+    user_posts_list = Post.objects.filter(author=request.user)
+    blog_settings = BlogSettings.objects.first()
+
+    if request.method == 'POST':
+        form = BlogSettingsForm(request.POST)
+        if form.is_valid():
+            num_pages = form.cleaned_data['num_pages']
+            blog_settings.num_pages = num_pages
+            blog_settings.save()
+            messages.success(request, 'Number of pages successfully updated.')
+            return redirect('users:dashboard')
+    else:
+        form = BlogSettingsForm(initial={'num_pages': blog_settings.num_pages})
+
+    context = {
+        'messages_list': messages_list,
+        'posts_list': posts_list,
+        'users_list': users_list,
+        'user_posts_list': user_posts_list,
+        'blog_settings': blog_settings,
+        'form': form
+    }
+    return render(request, 'users/users_dashboard.html', context)
+
+
+def users_action(request, action=None, pk=None):
+    if not request.user.is_superuser:
+        raise Http404
+    user = get_object_or_404(User, pk=pk)
+    if action == 0:
+        user.is_staff = not user.is_staff
+        user.save()
+    elif action == 1:
+        user.is_superuser = not user.is_superuser
+        user.save()
+    messages.success(request, 'Action successfully done.')
+    return redirect('users:dashboard')
 
 
 @login_required
